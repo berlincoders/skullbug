6dd8290651f387ec79582c194a99a7744d2b46d6
Romeo <ralonsoad@gmail.com>
2024-07-25 23:27:25 +0200

    Delete method

--- src/main/java/dev/skullbug/demo/skullbug/SkullbugController.java
+++ src/main/java/dev/skullbug/demo/skullbug/SkullbugController.java
@@ -1,6 +1,7 @@
 package dev.skullbug.demo.skullbug;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +37,11 @@ public class SkullbugController {
         if (bug == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return bug;
     }
+
+    @DeleteMapping("/bugs/{id}")
+    public void delete(@PathVariable String id){
+        Bug bug = db.remove(id);
+        if (bug == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
 }
+
