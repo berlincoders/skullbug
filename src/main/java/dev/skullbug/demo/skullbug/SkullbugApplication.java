e0231f8350018d9063424465fea15673f5f5de24
Romeo <ralonsoad@gmail.com>
2024-07-25 14:11:38 +0200

    Initial Project Setup

---
+++ src/main/java/dev/skullbug/demo/skullbug/SkullbugApplication.java
@@ -0,0 +1,13 @@
+package dev.skullbug.demo.skullbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkullbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkullbugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/skullbug/demo/skullbug/SkullbugApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skullbug.demo.skullbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkullbugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
