db8198807d594069fd701c04faef609c7a45ff85
Romeo <ralonsoad@gmail.com>
2024-07-29 18:13:27 +0200

    Running Version , with Model

--- src/main/java/dev/skullbug/demo/skullbug/User.java
+++ src/main/java/dev/skullbug/demo/skullbug/User.java
@@ -1,12 +1,12 @@
-package com.example.demo.model;
+package dev.skullbug.demo.skullbug;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -45,4 +45,29 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
